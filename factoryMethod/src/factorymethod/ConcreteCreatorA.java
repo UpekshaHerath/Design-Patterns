9ecbe320ed66cdc0b51e53ec12a4984a284c4b12
Upeksha Herath <upekshadilshan000@gmail.com>
2023-11-17 14:10:36 +0530

    explanation added

--- factoryMethod/src/factorymethod/ConcreteCreatorA.java
+++ factoryMethod/src/factorymethod/ConcreteCreatorA.java
@@ -6,6 +6,6 @@ import data.Product;
 public class ConcreteCreatorA extends Creator {
     @Override
     public Product factoryMethod() {
-        return new ConcreteProductA();
+        return new ConcreteProductA(); // if we need to change just change this return. No need to go and change the client code.
     }
 }

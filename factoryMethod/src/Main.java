25db43007f58fbe47d935965894351581dba5401
Upeksha Herath <upekshadilshan000@gmail.com>
2023-11-17 12:26:48 +0530

    description

--- factoryMethod/src/Main.java
+++ factoryMethod/src/Main.java
@@ -5,6 +5,11 @@ import factorymethod.Creator;
 
 public class Main {
     public static void main(String[] args) {
+        /***
+         * If you need to change the implementation, go to the relevant creator class and change the returning object. That's it.
+         * Otherwise, you have to change all the objects from place to place by finding them.
+         * No need to change the main method code(client side code not only the main method). That's the beauty.
+         */
         Creator creator = new ConcreteCreatorA(); // create a creator object which can use to create product_A
         Product product = creator.factoryMethod();
         product.checkProduct();
